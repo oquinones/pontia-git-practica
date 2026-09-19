675de104367685dddb003736afb4a4a4881ffc96
Orlando Quinones <oquinones1@gmail.com>
2026-09-19 19:35:19 +0200

    feat(): update operations.py file with subtraction

--- operations.py
+++ operations.py
@@ -1,6 +1,11 @@
 def info():
     return "Operaciones básicas"
     
+    
+def resta(a: int, b: int):
+    return a - b
+    
 
 if __name__ == "__main__":
     print(info())
+    print(f"Resta de 3 y 4 = {resta(3, 4)}")
